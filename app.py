4676a45c2059c706c796ac6a8cb45d48abc84a7a
Tejas Phirke <41693514+tejasphirke@users.noreply.github.com>
2020-09-09 15:24:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,10 @@ def index():
         #[{'name': 'sandeep', 'date': '31-09-2020'}]
         date.append(data['date'])
     return render_template('stats.html', p1= names[0],d1=date[0], p2=names[1], d2=date[1],p3=names[2], d3=date[2],p4=names[3], d4=date[3])
+#https://sw0sm7rpq5.execute-api.us-east-1.amazonaws.com/sanitizerappinsert?Name=someone&date=someday
+# above url is the url to insert data 
+# https://7bwcdzfj44.execute-api.us-east-1.amazonaws.com/sanitizerappretrieve?Name=someone
+# above url is the url which is to retrieve
 
 
 if __name__ == '__main__':
